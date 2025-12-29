2cfb98b92358bd478be33679cc13287ecaa06f48
gkvanadil-ui <gkvanadil@gmail.com>
2025-12-29 12:29:11 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ios: {
+          blue: '#007AFF',
+          green: '#34C759',
+          gray: '#8E8E93',
+          bg: '#F2F2F7',
+          bubble: '#E9E9EB'
+        }
+      }
+    },
   },
   plugins: [],
 }

8cd4fe0caa2dbb93198a055063b954a838e05002
gkvanadil-ui <gkvanadil@gmail.com>
2026-01-01 12:51:41 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 1. 빌드 시 TypeScript 에러 무시 (배포 성공 최우선)
+  // 빌드 시 TypeScript 에러 무시
   typescript: {
     ignoreBuildErrors: true,
   },
-  // 2. 빌드 시 ESLint 에러 무시
+  // 빌드 시 ESLint 에러 무시
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // 3. (선택) React Strict Mode 해제 (두 번 렌더링 방지)
   reactStrictMode: false,
 };
 
